ea90907bc328b069919779d63d24a72abba57da0
Miki <makmel.miki@gmail.com>
2020-05-12 20:38:27 +0300

    elephant connection for heroku

---
+++ Server/elephantsqlUser.js
@@ -0,0 +1,11 @@
+// postgres://{db_username}:{db_password}@{host}:{port}/{db_name}
+
+const db_username = 'uyjsosov';
+const db_password = 'SL1FfYjnFAM_E4WgH_krDEvkMcNC10jd';
+const db_host = 'balarama.db.elephantsql.com';
+const db_port = '5432';
+const db_name = 'uyjsosov';
+
+const conString = `postgres://${db_username}:${db_password}@${db_host}:${db_port}/${db_name}`
+
+module.exports = conString;
