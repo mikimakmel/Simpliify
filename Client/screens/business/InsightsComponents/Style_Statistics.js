614ae579f484946148645a951efea2c9153371c3
Redbulbul <levaricohen@gmail.com>
2020-05-13 14:08:12 +0300

    Made Service Income Pie component

---
+++ Client/screens/business/InsightsComponents/Style_Statistics.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  itemStyle: {
+    width: 20,
+    height:20,
+    marginRight: 10,
+    marginLeft: 15,
+    marginTop: 2,
+  }
+})
