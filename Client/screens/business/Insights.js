f23b152540dd1df34fc0f0194fc31103085b15d2
Redbulbul <levaricohen@gmail.com>
2020-05-14 13:38:45 +0300

    Added Gender Distribution Pie

--- Client/screens/business/Insights.js
+++ Client/screens/business/Insights.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
 import { View, Text, SafeAreaView, ScrollView } from 'react-native';
-// import { Path, Text } from 'react-native-svg';
-// import * as shape from 'd3-shape';
-// import * as scale from 'd3-scale';
-// import ServiceIncome from './ServiceIncome';
+import ServiceIncome from './InsightsComponents/ServiceIncome';
+import GenderDistribution from './InsightsComponents/GenderDistribution';
 
 class Insights extends Component {
   constructor(props) {
     super(props);
     this.state =
     {
-      serviceincome: [{ name: ['Beauty Sales', 'Baby Legal'], amount: [50, 80], }],
       colors: ['#600080', '#9900cc', '#c61aff', '#d966ff', '#ecb3ff'],
+      serviceincome: [{ name: ['Beauty Sales', 'Baby Legal'], amount: [50, 80], }],
+      gendercount: [{ gender: ['Female', 'Male'], amount: [100, 50] }],
     };
   }
 
+
   render() {
-    return (
-      <SafeAreaView style={{flex: 1}}>
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={{fontSize: 32}}> Insights </Text>
-        </View>
+    return(
+      <SafeAreaView>
+         <ScrollView>
+<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+<Text style={{fontSize: 32}}> Insights </Text>
+</View>
+            {/* <ServiceIncome
+              serviceincome = {this.state.serviceincome}
+              colors = {this.state.colors}
+            />
+            <GenderDistribution
+              gendercount = {this.state.gendercount}
+              colors = {this.state.colors}
+            /> */}
+        </ScrollView>
       </SafeAreaView>
-    );
-    // return(
-    //   <SafeAreaView>
-    //      <ScrollView>
-    //         <ServiceIncome
-    //           serviceincome = {this.state.serviceincome}
-    //           colors = {this.state.colors}
-    //         />
-    //     </ScrollView>
-    //   </SafeAreaView>
-    // )
-  }
+    )
+  }  
 }
 
 export default Insights;
