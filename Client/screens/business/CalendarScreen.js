cd1517a675736401d254cc6c843d95f65ca31d97
Redbulbul <levaricohen@gmail.com>
2020-05-27 17:23:27 +0300

    Calendar screen shows proper (hard coded) data

--- Client/screens/business/CalendarScreen.js
+++ Client/screens/business/CalendarScreen.js
@@ -2,59 +2,212 @@ import React, { Component } from 'react';
 import { Text, Alert, TouchableOpacity, SafeAreaView, StatusBar } from 'react-native';
 import { Agenda } from 'react-native-calendars';
 import { ListItem, Rating, Overlay } from 'react-native-elements';
+import { View } from 'react-native-animatable';
+import PhoneCall from 'react-native-phone-call';
+import { Popup } from 'react-native-map-link';
+var moment = require('moment');
 import colors from '../../constants/Colors';
 import styles from '../../styles/business/Style_CalendarScreen';
-import { View } from 'react-native-animatable';
-import { AntDesign, FontAwesome, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
 class CalendarScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       items: {},
+      isPopupVisble: false,
+      orders: [
+        {
+              "status": "Cancelled",
+              "starttime": "2020-04-17T01:38:22.000Z",
+              "endtime": "2020-04-16T23:08:22.000Z",
+              "name": "Jarvis Flatt",
+              "service": "Sports Services",
+              "phone": "0548843282",
+              "address": "15 Yetzira, Rehovot"
+          },
+          {
+              "status": "Success",
+              "starttime": "2020-04-17T08:16:07.000Z",
+              "endtime": "2020-04-17T05:46:07.000Z",
+              "name": "Cristin Jeremiah",
+              "service": "Beauty Product Management",
+              "phone": "0502996001",
+              "address": "5 Hashalom, Tel Aviv"
+          },
+          {
+              "status": "Success",
+              "starttime": "2020-04-18T07:22:03.000Z",
+              "endtime": "2020-04-18T05:52:03.000Z",
+              "name": "Claudianus Peealess",
+              "service": "Jewelery Legal",
+              "phone": "0504512360",
+              "address": "98 Rabbi Akiva, Bnei Brak"
+          },
+          {
+              "status": "Success",
+              "starttime": "2020-05-05T14:00:00.000Z",
+              "endtime": "2020-05-05T12:00:00.000Z",
+              "name": "Sarena Blinde",
+              "service": "Grocery Legal",
+              "phone": "0506156522",
+              "address": "18 Hachalutzim, Tel Aviv"
+          },
+          {
+              "status": "Success",
+              "starttime": "2020-05-05T15:00:00.000Z",
+              "endtime": "2020-05-05T13:00:00.000Z",
+              "name": "Sarena Blinde",
+              "service": "Grocery Legal",
+              "phone": "0506156522",
+              "address": "18 Hachalutzim, Tel Aviv"
+          },
+          {
+              "status": "Cancelled",
+              "starttime": "2020-05-05T16:56:59.000Z",
+              "endtime": "2020-05-05T14:26:59.000Z",
+              "name": "Carry Damarell",
+              "service": "Sports Research and Development",
+              "phone": "0505965707",
+              "address": "94 Em Hamoshavot, Petah Tikva"
+          },
+          {
+              "status": "Success",
+              "starttime": "2020-05-11T14:09:55.000Z",
+              "endtime": "2020-05-11T12:39:55.000Z",
+              "name": "Dulciana Humpage",
+              "service": "Music Legal",
+              "phone": "0504799763",
+              "address": "15 Yetzira, Rehovot"
+          },
+          {
+              "status": "Success",
+              "starttime": "2020-05-23T13:00:00.000Z",
+              "endtime": "2020-05-23T11:30:00.000Z",
+              "name": "Miki Makmel",
+              "service": "Jewelery Legal",
+              "phone": "0525368689",
+              "address": "HaKishon, Tel Aviv"
+          },
+          {
+              "status": "Success",
+              "starttime": "2020-05-23T14:30:00.000Z",
+              "endtime": "2020-05-23T13:00:00.000Z",
+              "name": "Miki Makmel",
+              "service": "Jewelery Legal",
+              "phone": "0525368689",
+              "address": "HaKishon, Tel Aviv"
+          },
+          {
+              "status": "Success",
+              "starttime": "2020-05-23T16:09:01.000Z",
+              "endtime": "2020-05-23T14:09:01.000Z",
+              "name": "Lovell Sellor",
+              "service": "Kids Training",
+              "phone": "0544595363",
+              "address": "12 Harab Lewin, Afula"
+          },
+          {
+              "status": "Success",
+              "starttime": "2020-05-24T06:51:50.606Z",
+              "endtime": "2020-05-24T05:21:50.606Z",
+              "name": "Miki Makmel",
+              "service": "Jewelery Legal",
+              "phone": "0525368689",
+              "address": "HaKishon, Tel Aviv"
+          },
+          {
+              "status": "Cancelled",
+              "starttime": "2020-05-25T13:20:21.696Z",
+              "endtime": "2020-05-25T11:50:21.696Z",
+              "name": "Miki Makmel",
+              "service": "Jewelery Legal",
+              "phone": "0525368689",
+              "address": "HaKishon, Tel Aviv"
+          },
+          {
+              "status": "Confirmed",
+              "starttime": "2020-05-26T12:00:00.000Z",
+              "endtime": "2020-05-26T10:30:00.000Z",
+              "name": "Miki Makmel",
+              "service": "Music Legal",
+              "phone": "0525368689",
+              "address": "HaKishon, Tel Aviv"
+          },
+          {
+              "status": "Success",
+              "starttime": "2020-05-26T13:30:00.000Z",
+              "endtime": "2020-05-26T12:00:00.000Z",
+              "name": "Miki Makmel",
+              "service": "Music Legal",
+              "phone": "0525368689",
+              "address": "HaKishon, Tel Aviv"
+          },
+          {
+              "status": "Confirmed",
+              "starttime": "2020-05-28T15:00:00.000Z",
+              "endtime": "2020-05-28T13:30:00.000Z",
+              "name": "Miki Makmel",
+              "service": "Jewelery Legal",
+              "phone": "0525368689",
+              "address": "HaKishon, Tel Aviv"
+          },
+          {
+              "status": "Confirmed",
+              "starttime": "2020-05-28T16:30:00.000Z",
+              "endtime": "2020-05-28T15:00:00.000Z",
+              "name": "Miki Makmel",
+              "service": "Jewelery Legal",
+              "phone": "0525368689",
+              "address": "HaKishon, Tel Aviv"
+        }]
     };
   }
 
   componentDidMount() {
+    this.fetchAllOrders();
+  }
 
+  fetchAllOrders() {
+    let temp = this.state.items;
+    this.state.orders.map((item) => {
+      let start = moment(item.starttime).format('HH:mm')
+      let end = moment(item.endtime).format('HH:mm')
+      let duration = end.concat(' - ', start)
+
+      if(!temp[moment(item.starttime).format('YYYY-MM-DD')])
+        temp[moment(item.starttime).format('YYYY-MM-DD')] = []
+
+        temp[moment(item.starttime).format('YYYY-MM-DD')].push({
+          name: item.name,
+          service: item.service,
+          time: duration,
+          status: item.status,
+          phone: item.phone,
+          address: item.address  
+        })
+      
+    })
+      this.setState({items: temp})
   }
 
   loadItems(day) {
     setTimeout(() => {
+      const newItems = this.state.items;
       for (let i = -15; i < 85; i++) {
         const time = day.timestamp + i * 24 * 60 * 60 * 1000;
         const strTime = this.timeToString(time);
-        if (!this.state.items[strTime]) {
-          this.state.items[strTime] = [];
-            this.state.items[strTime].push({
-              name: 'Miki Makmel',
-              service: 'Haircut',
-              time: '14:30 - 16:00',
-              status: 'Success',
-              secret: 'This thing took too much time...'
-            });
-            this.state.items[strTime].push({
-              name: 'Lev Ari',
-              service: 'Haircut',
-              time: '14:30 - 16:00',
-              status: 'Cancelled',
-              secret: 'This thing took too much time...'
-            });
-            this.state.items[strTime].push({
-              name: 'Shira Star',
-              service: 'Yoga',
-              time: '17:30 - 17:31',
-              status: 'Confirmed',
-              secret: 'This thing took too much time...'
-            });
+        if (!newItems[strTime]) {
+          newItems[strTime] = [];
         }
       }
-      const newItems = {};
-      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
-      this.setState({
-        items: newItems
-      });
+      Object.keys(newItems).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({items: newItems});
     }, 1000);
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
   }
 
   renderItem(item) {
@@ -69,7 +222,26 @@ class CalendarScreen extends Component {
     return (
       <TouchableOpacity 
         style={[styles.item, {opacity: item.status !== 'Confirmed' ? 0.8 : 1}]} 
-        onPress={() => Alert.alert(item.secret)}
+        onPress={() => Alert.alert('Contact Customer', '',
+        [
+            {
+              text: 'Call - ' + item.phone,
+              onPress: () => {
+              const args = {
+                number: item.phone,
+                prompt: false
+              }
+              PhoneCall(args).catch(console.error)
+            }
+            },
+            {
+              text: 'Navigate - ' + item.address,
+              onPress: () => this.setState({ isPopupVisble: true })
+            },
+            { text: 'Cancel', onPress: () => console.log('OK Pressed'), style: 'cancel' }
+        ],
+        { cancelable: false }
+        )} 
       >
         <ListItem
           title={item.service}
@@ -80,6 +252,22 @@ class CalendarScreen extends Component {
         />
         <View style={{backgroundColor: statusColor, height: 2, width: '90%', borderRadius: 10, alignSelf: 'center', marginTop: 5}}/>
         {item.status === 'Confirmed' ? null : this.renderStatusIcon(item)}
+      <Popup
+            isVisible={this.state.isPopupVisble}
+            onCancelPressed={() => this.setState({ isPopupVisble: false })}
+            onAppPressed={() => this.setState({ isPopupVisble: false })}
+            onBackButtonPressed={() => this.setState({ isPopupVisble: false })}
+            modalProps={{ animationIn: 'slideInUp' }}
+            appsWhiteList={[]}
+            options={{
+              alwaysIncludeGoogle: true,
+              latitude: 32.1563346,
+              longitude: 34.8814187,
+              title: item.address,
+              dialogTitle: `${item.address}`,
+              cancelText: 'Cancel'
+            }}
+          />
       </TouchableOpacity>
     );
   }
@@ -87,25 +275,18 @@ class CalendarScreen extends Component {
   renderStatusIcon(item) {
     if(item.status === 'Cancelled') {
       return(
-        <View style={{position: 'absolute', top: 5, alignSelf: 'center'}}>
-          {/* <MaterialCommunityIcons name="cancel" size={20} color={colors.red} style={{}}/> */}
-          <Text style={{fontSize: 10, fontWeight: '300', color: colors.red}}>Cancelled</Text>
+        <View style={styles.statusIcon}>
+          <Text style={styles.statusText, {color: colors.red}}>Cancelled</Text>
         </View>
       )
     }
-    else if(item.status === 'Success') {
+    if(item.status === 'Success') {
       return(
-        <View style={{position: 'absolute', top: 5, alignSelf: 'center'}}>
-          {/* <AntDesign name="creditcard" size={20} color={colors.green03} style={{}}/> */}
-          <Text style={{fontSize: 10, fontWeight: '300', color: colors.green03}}>Paid</Text>
+        <View style={styles.statusIcon}>
+          <Text style={styles.statusText, {color: colors.green03}}>Paid</Text>
         </View>
       )
     }
-  }
-
-  timeToString(time) {
-    const date = new Date(time);
-    return date.toISOString().split('T')[0];
   }
 
   render() {
--- Client/styles/business/Style_CalendarScreen.js
+++ Client/styles/business/Style_CalendarScreen.js
@@ -5,12 +5,21 @@ export default StyleSheet.create({
     paddingTop: 5,
     height: 350
   },
-    item: {
+  item: {
     backgroundColor: 'white',
     flex: 1,
     borderRadius: 15,
     padding: 10,
     marginRight: 10,
     marginTop: 17
+  },
+  statusIcon: {
+    position: 'absolute',
+    top: 5,
+    alignSelf: 'center'
+  },
+  statusText: {
+    fontSize: 10,
+    fontWeight: '300'
   }
-})
+  })
