d60ae1f0b67f7da2ea380be1d2d6bd107faf6854
Miki <makmel.miki@gmail.com>
2020-05-30 21:56:04 +0300

    Business form

--- Server/controllers/service.ctl.js
+++ Server/controllers/service.ctl.js
@@ -51,7 +51,7 @@ module.exports = {
 
         const query = 
             `INSERT INTO Service 
-            (BusinessID, Name, Price, DurationMinutes, Qouta)
+            (BusinessID, Name, Price, DurationMinutes, Quota)
             VALUES 
             (${BusinessID}, '${name}', ${price}, ${durationMinutes}, ${qouta})
             RETURNING *`;
