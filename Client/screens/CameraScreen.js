fbfd718ae2e6784d0b4facc783185a6a332cd4c2
Miki <makmel.miki@gmail.com>
2020-07-05 11:08:02 +0300

    Diagrams

--- Client/screens/CameraScreen.js
+++ Client/screens/CameraScreen.js
@@ -38,8 +38,7 @@ class CameraScreen extends Component {
         };
         formData.append('image', image);
 
-        // const url = 'http://192.168.1.198:5000/pic';
-        const url = 'http://ec2-18-218-164-196.us-east-2.compute.amazonaws.com:5000/pic';
+        const url = 'http://ec2-18-222-226-54.us-east-2.compute.amazonaws.com:5000/pic';
         const options = { method: 'POST', body: formData };
         const request = new Request(url, options);
 
