2497e7a06f4fb05a00b9c38f93257e08debcf09c
Miki <makmel.miki@gmail.com>
2020-08-06 17:04:07 +0300

    fixx

--- Client/screens/CameraScreen.js
+++ Client/screens/CameraScreen.js
@@ -38,7 +38,7 @@ class CameraScreen extends Component {
         };
         formData.append('image', image);
 
-        const url = 'http://192.168.1.198:5000/pic';
+        const url = 'http://127.0.0.1:5000/pic';
         const options = { method: 'POST', body: formData };
         const request = new Request(url, options);
 
